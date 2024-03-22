99c9033cbb1d744f887d9b92f4914005f0ea8b83
gat234 <131648781+gat234@users.noreply.github.com>
2024-03-22 11:08:20 +0200

    Add files via upload

--- scripts/buttonSearch.js
+++ scripts/buttonSearch.js
@@ -7,6 +7,10 @@ async function mouse(){
 onmousemove = function(e){
     var r = document.querySelector('.foot');
     var rs = getComputedStyle(r);
+
+    let width = rs.getPropertyValue("--widthy")
+    let height = rs.getPropertyValue("--heighty")
+    console.log(width)
     r.style.setProperty("--lefty",`${e.clientX-50}px`)
     r.style.setProperty("--righty",`${e.clientY-50}px`)
 }
